343879a28f7061ba84cc0386dfdb1c86e2bc672d
Animesh <akjha6680@gmail.com>
2020-06-13 22:27:58 +0530

    fix PORT error

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ function isLoggedIn(req, res, next){
 
 
 
-app.listen( 3000||process.env.PORT , process.env.IP, ()=>{
+app.listen( process.env.PORT||3000 , process.env.IP, ()=>{
 	console.log("app is running!")
 })
