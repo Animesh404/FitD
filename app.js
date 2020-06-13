7bf2f0af88320774615587bd8a2e99a252686d84
Animesh <akjha6680@gmail.com>
2020-06-13 22:23:41 +0530

    add heroku custom port option

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ function isLoggedIn(req, res, next){
 
 
 
-app.listen( 3000, process.env.IP, ()=>{
+app.listen( 3000||process.env.PORT , process.env.IP, ()=>{
 	console.log("app is running!")
 })
